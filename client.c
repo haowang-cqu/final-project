48e22f38c40aa6c60ca84723e675f044391a01fd
Hao Wang <iamwh.cn@gmail.com>
2021-12-29 19:50:30 +0800

    新建服务端和客户端

---
+++ client.c
@@ -0,0 +1,13 @@
+/**
+ * 客户端
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char **argv)
+{
+    
+    exit(0);
+}
---
+++ server.c
@@ -0,0 +1,13 @@
+/**
+ * 服务端
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char **argv)
+{
+    
+    exit(0);
+}
